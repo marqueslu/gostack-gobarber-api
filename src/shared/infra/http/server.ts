ca878c689268de2b0292c02626b1fd5550e7e565
Lucas Marques <lucas.msilva09@gmail.com>
2020-07-19 20:17:45 -0300

    Changing the project architecture

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,11 +5,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadconfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadconfig from './config/upload';
 
-import './database';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
 
 const app = express();
 app.use(cors());
