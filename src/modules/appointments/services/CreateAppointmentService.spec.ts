1f8738b7add3bd57e5d52eca5b01575aa20ed0f5
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-03 22:39:15 -0300

    Refactoring all the Appointment services and tests to includes the user_id property

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointmentService.execute({
       date: new Date(),
+      user_id: '12313',
       provider_id: '123123123',
     });
 
@@ -28,12 +29,14 @@ describe('CreateAppointment', () => {
 
     await createAppointmentService.execute({
       date: appointmentDate,
+      user_id: '12313',
       provider_id: '123123123',
     });
 
     expect(
       createAppointmentService.execute({
         date: appointmentDate,
+        user_id: '12313',
         provider_id: '123123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
