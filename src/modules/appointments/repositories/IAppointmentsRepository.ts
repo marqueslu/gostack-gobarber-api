dc1064950b0e3f9c37d9e3be154c11f22f26e361
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-03 22:19:48 -0300

    Listing the provider availability

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,15 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dto/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dto/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dto/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
