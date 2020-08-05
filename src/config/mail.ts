ee522a98491bba0719eb0fc1137c41b511ea0001
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-05 12:26:45 -0300

    Including a new mail provider

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'lucas@teste.com.br',
+      name: 'Lucas Marques',
+    },
+  },
+} as IMailConfig;
