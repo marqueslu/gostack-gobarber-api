8f92aa70bbeb0785e41884a3389f39d082fe0c0e
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-05 17:41:18 -0300

    Adding the s3 storage provider for file upload

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+import aws, { S3 } from 'aws-sdk';
+import uploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+export default class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+        ContentDisposition: `inline; filename=${file}`,
+      })
+      .promise();
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: 'app-marques-gobarber-2',
+        Key: file,
+      })
+      .promise();
+
+    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
